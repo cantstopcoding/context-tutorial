6068800a1eadccb73c7c78c4f49efeb515a660d9
cantstopcoding <jonathan_koss@yahoo.com>
2022-04-08 14:53:59 -0400

    create and return ChildCardComponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ChildCardComponent />
+    </>
+  );
+}
+
+function ChildCardComponent() {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Child Card Component</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
 export default App;
