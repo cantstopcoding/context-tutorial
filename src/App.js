a7095cc64ddff8553d5571149c76d668e9236c3d
cantstopcoding <jonathan_koss@yahoo.com>
2022-04-08 16:23:30 -0400

    create themes object

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 
 function App() {
+  const themes = {
+    green: {
+      foreground: "#ffffff",
+      background: "#008000",
+    },
+    red: {
+      foreground: "#ffffff",
+      background: "#FF0000",
+    },
+  };
   return (
     <>
       <ChildCardComponent />
